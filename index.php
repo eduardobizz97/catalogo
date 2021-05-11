15c2bfdb58c6a4fef047d97de2a8ffafe1c4b34a
EDUARDO JOSE BIZZARRO MATA <eduardo.bizzarro@inacapmail.cl>
2021-05-11 07:24:47 -0400

    Add routes

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php
     require_once("db/Conexion.php");
     require_once("models/Cliente.php");
+    include('./routes/Route.php');
     // require_once("controllers/personas_controller.php");
+    Route::add('/proyecto-catalogo/index.php', function(){
+        $cliente = new Cliente();
+        $cliente->getClientes();
+    });
 
-    $cliente = new Cliente();
+    Route::run("/");
+    if($_SERVER['REQUEST_URI']== '/proyecto-catalogo/clientes.php'){
+        
+    }
 ?>
--- models/Cliente.php
+++ models/Cliente.php
@@ -2,15 +2,24 @@
 
 class Cliente {
 
-   private $db;
-   private $cliente;
-
-    public function __construct(){
-        $conect = new Conexion();
-        $cliente = $conect->prepare('select * from clientes');
-        echo json_encode($cliente->fetch());   
-
-    }
+    private $db;
+    private $idCliente;
+    private $nombre;
+    private $correo;
+    private $contrasena;
+ 
+     public function __construct(){
+        $db = new Conexion();
+        $this->db = $db;     
+     }
+ 
+     public function getClientes() {
+        $sql = $this->db->prepare("select * from clientes");
+        $json = $sql->fetch(PDO::FETCH_ASSOC);
+        echo json_encode($json);
+        echo $_SERVER['REQUEST_URI'];
+        
+     }
 
 
 }
